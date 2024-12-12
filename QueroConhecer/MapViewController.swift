73494d0619cb9cd78bed502d436cda47d1d4fd55
edy alves <30.edyalves@gmail.com>
2024-12-12 10:49:09 -0300

    implementacao de pesquisa com searchBar

--- QueroConhecer/MapViewController.swift
+++ QueroConhecer/MapViewController.swift
@@ -16,14 +16,18 @@ class MapViewController: UIViewController {
     @IBOutlet weak var viInfo: UIView!
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbAdress: UILabel!
+    @IBOutlet weak var loadingMap: UIActivityIndicatorView!
     
     var places: [Place]!
+    var poi: [MKAnnotation] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.mapType = .mutedStandard
         searchBar.isHidden = true
         viInfo.isHidden = true
+        
+        mapView.delegate = self
         
         if places.count == 1 {
             title = places[0].name
@@ -41,10 +45,9 @@ class MapViewController: UIViewController {
     
     func addToMap(_ place: Place) {
         //adicionando alfinetes em cada ponto salvo no map
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = place.coordinate
+        let annotation = PlaceAnotation(coordinate: place.coordinate, type: .place)
         annotation.title = place.name
-        annotation.subtitle = place.adress
+        annotation.address = place.adress
         mapView.addAnnotation(annotation)
     }
     
@@ -56,6 +59,8 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+        searchBar.resignFirstResponder()
+        searchBar.isHidden = !searchBar.isHidden
     }
     /*
     // MARK: - Navigation
@@ -67,4 +72,64 @@ class MapViewController: UIViewController {
     }
     */
 
+}
+
+extension MapViewController:MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is PlaceAnotation) {
+            return nil
+        }
+        let type = (annotation as! PlaceAnotation).type
+        let identifier = "\(type)"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.markerTintColor = .red
+        }
+        annotationView?.annotation = annotation
+        annotationView?.canShowCallout = true
+        annotationView?.markerTintColor = type == .place ? UIColor(.blue.opacity(0.5)) : UIColor(.red.opacity(0.5))
+        annotationView?.glyphImage = type == .place ? UIImage(systemName: "airplane") : UIImage(systemName: "photo.artframe")
+        annotationView?.displayPriority = type == .place ? .required : .defaultHigh
+        return annotationView
+    }
+}
+
+extension MapViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.isHidden = true
+        // resignFirstResponder() para recuar o teclado após a busca
+        searchBar.resignFirstResponder()
+        loadingMap.startAnimating()
+        
+        //criando a requisicao
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        
+        //criando o obj
+        let search = MKLocalSearch(request: request)
+        search.start {
+            response,
+            error in
+            if error == nil {
+                guard let response = response else {
+                    self.loadingMap.stopAnimating()
+                    return
+                }
+                self.mapView.removeAnnotations(self.poi)
+                self.poi.removeAll()
+                for item in response.mapItems {
+                    let annotation = PlaceAnotation(coordinate: item.placemark.coordinate, type: .poi)
+                    annotation.title = item.name
+                    annotation.subtitle = item.phoneNumber
+                    annotation.address = Place.getFormattedAdress(with: item.placemark)
+                    self.poi.append(annotation)
+                }
+                self.mapView.addAnnotations(self.poi)
+                self.mapView.showAnnotations(self.poi, animated: true)
+            }
+            self.loadingMap.stopAnimating()
+        }
+    }
 }
--- QueroConhecer/PlaceAnotation.swift
+++ QueroConhecer/PlaceAnotation.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import MapKit
+
+class PlaceAnotation: NSObject, MKAnnotation {
+    
+    enum PlaceType {
+        case place
+        case poi
+    }
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    var type: PlaceType
+    var address: String?
+    
+    init(coordinate: CLLocationCoordinate2D, type: PlaceType) {
+        self.coordinate = coordinate
+        self.type = type
+    }
+    
+}
